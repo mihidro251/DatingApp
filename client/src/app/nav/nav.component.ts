dc8e43be294369f1a89fd5c84f41d5a064824355
Miguel Lopes <miguel.lopes4@vodafone.com>
2023-08-02 15:45:13 +0100

    End of Section 5

---
+++ client/src/app/nav/nav.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Observable, of } from 'rxjs';
+import { User } from '../_models/user';
+import { AccountService } from '../_services/account.service';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css']
+})
+export class NavComponent implements OnInit {
+    
+  model: any = {};
+  
+
+  constructor(public accountService: AccountService) { }
+
+  ngOnInit(): void {
+
+    
+  }
+
+
+  login() {
+    this.accountService.login(this.model).subscribe({
+
+      next: response => {
+        console.log(response);
+       
+      },
+      error: error => console.log(error)
+
+    })
+  }
+
+  logout() {
+
+    this.accountService.logout();
+
+    
+  }
+
+}
