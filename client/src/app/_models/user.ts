8f0e25f7b9a897d267c2e0498cfabf94ea86510e
Miguel Lopes <miguel.lopes4@vodafone.com>
2023-09-05 16:29:35 +0100

    End of Section 9

--- client/src/app/_models/user.ts
+++ client/src/app/_models/user.ts
@@ -1,6 +1,6 @@
 export interface User {
-
   username: string;
   token: string;
-
 }
+
+
--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -10,12 +10,12 @@ export class HomeComponent implements OnInit{
   registerMode = false;
   users: any;
 
-  constructor(private http: HttpClient) { }
+  constructor() { }
 
 
 
     ngOnInit(): void {
-      this.getUsers();
+    
     }
 
   registerToggle() {
@@ -24,14 +24,7 @@ export class HomeComponent implements OnInit{
   }
 
 
-  getUsers() {
-    this.http.get("https://localhost:5001/api/users").subscribe({
-      next: response => this.users = response,
-      error: error => console.log(error),
-      complete: () => console.log("Request has completed")
-    })
-
-  }
+ 
 
   cancelRegisterMode(event: boolean) {
     this.registerMode = event;
